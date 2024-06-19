8860e3885f8b18c9b42de128fbfc696a0deca841
Kvothek88 <115589647+Kvothek88@users.noreply.github.com>
2024-06-19 23:08:27 +0300

    Update Grid.java

--- src/Grid.java
+++ src/Grid.java
@@ -4,7 +4,6 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Scanner;
 import java.lang.Math;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
