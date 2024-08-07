72bdc572648978849f3202f69c38bd67117878db
Kvothek88 <115589647+Kvothek88@users.noreply.github.com>
2024-08-07 17:00:54 +0300

    Update MineFrame.java

--- src/MineFrame.java
+++ src/MineFrame.java
@@ -1,25 +1,50 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 
 public class MineFrame extends JFrame {
 	
+	private Grid board;
+	
 	MineFrame(Grid board,String dif){
+		
+		this.board=board;
 		
 	    setTitle("Minesweeper");
 	    if(dif=="easy") {
-	    	setSize(493, 576);
+	    	setSize(493, 597);
 	    }
 	    else if(dif=="medium") {
-	    	setSize(496, 578);
+	    	setSize(496, 601);
 	    }
 	    else {
-	    	setSize(586, 668);
+	    	setSize(586, 692);
 	    }
 	    
 	    add(board);
+	    
 	    
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setLocationRelativeTo(null);
 	    setVisible(true);
 	}
+	
+	public void resetSize(String dif) {
+	    if(dif=="easy") {
+	    	setSize(493, 597);
+	    }
+	    else if(dif=="medium") {
+	    	setSize(496, 601);
+	    }
+	    else {
+	    	setSize(586, 692);
+	    }
+	}
 }
+
+
